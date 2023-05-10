6f495b3721e0df336d4ec6e58a059e45ed9aeb0a
FidaaAbuGalion <118681325+FidaaAbuGalion@users.noreply.github.com>
2023-05-10 13:16:15 +0300

    the end

--- src/Constants.java
+++ src/Constants.java
@@ -1,7 +1,7 @@
 public class Constants {
-    public static final String PATH_TO_DATA_FILE = "src/main/resources/titanic.csv";
+
     public static final String PATH_TO_STATISTICS = "src/main/resources/Statistic.txt";
-    public static final String SOURCE_JPG = "/TitanicBG.png";
+    //
     public static final int WINDOW_WIDTH = 600;
     public static final int WINDOW_HEIGHT = 700;
     public static final String[] PASSENGER_CLASS_OPTIONS = { "All", "1st", "2nd", "3rd"};
@@ -15,22 +15,23 @@ public class Constants {
     public static final int MIDDLE_WIDTH = WINDOW_WIDTH / 5;
     public static final int TITLE_WIDTH = 400;
     public static final int MARGIN_TITLE_FROM_TOP = 5;
-
+    //
     public static final int TITLE_HEIGHT = 90;
     public static final int LABEL_WIDTH = 110;
     public static final int LABEL_HEIGHT = 30;
     public static final int COMBO_BOX_WIDTH = 80;
     public static final int COMBO_BOX_HEIGHT = 30;
-
+    //
     public static  final int WIDTH_BUTTON = 100;
-    public static  final int HEIGHT_BUTTON = 50;
 
+    public static  final int HEIGHT_BUTTON = 50;
+    //
     public static final int INITIALIZE = 0;
     public static final int INDEX_0 = 0;
     public static final int INDEX_1 = 1;
     public static final int INDEX_2 = 2;
     public static final int INDEX_3 = 3;
-
+    //
     public static final int ZERO_AGE = 0;
     public static final double TEN_AGE = 10.9;
     public static final int ELEVEN_AGE = 11;
@@ -42,7 +43,7 @@ public class Constants {
     public static final int FORTY_ONE = 41;
     public static final double FIFTY_AGE = 50.9;
     public static final int FIFTY_ONE_AGE = 51;
-
+    //
     public static final double TEN_POUNDS = 10.9;
     public static final int ELEVEN_POUNDS = 11;
     public static final int THIRTY_POUNDS = 30;
